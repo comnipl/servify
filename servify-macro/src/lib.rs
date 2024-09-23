4c23b81ab4c3c65dc285110d969832c3dd71fa52
AsPulse / あすぱる <contact@aspulse.dev>
2024-09-24 08:04:50 +0900

    add proc_macro2

--- servify-macro/src/lib.rs
+++ servify-macro/src/lib.rs
@@ -1,3 +1,13 @@
-pub fn export(attrs: TokenStream, item: TokenStream) -> TokenStream {
+use proc_macro2::TokenStream;
+
+#[proc_macro_attribute]
+pub fn export(
+    attrs: proc_macro::TokenStream,
+    item: proc_macro::TokenStream,
+) -> proc_macro::TokenStream {
+    impl_export(attrs.into(), item.into()).into()
+}
+
+fn impl_export(attrs: TokenStream, item: TokenStream) -> TokenStream {
     item
 }
