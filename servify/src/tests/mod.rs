ddeaea49cd0bbfef31a6e1fca5d3dd71c66f9de4
AsPulse / あすぱる <contact@aspulse.dev>
2024-09-25 07:30:48 +0000

    add property name parsing

--- servify/src/tests/mod.rs
+++ servify/src/tests/mod.rs
@@ -1,1 +1,50 @@
 mod expanded_1;
+
+
+
+#[servify_macro::export]
+impl A {
+    fn increment(&mut self, count: u32) -> u32 {
+        self.a += count;
+        self.a
+    }
+}
+
+#[servify_macro::service(
+    impls = (
+        a_increment
+    )
+)]
+struct B {
+
+}
+
+mod A {
+    use super::a_increment;
+
+    pub struct Server {
+        pub a: u32,
+    }
+    #[derive(Clone)]
+    pub struct Client {
+        tx: tokio::sync::mpsc::Sender<Message>,
+    }
+
+    pub enum Message {
+        Increment(
+            a_increment::Request,
+            tokio::sync::oneshot::Sender<a_increment::Response>,
+        ),
+    }
+
+    #[doc(hidden)]
+    pub async fn __internal_increment(
+        client: &Client,
+        req: a_increment::Request,
+    ) -> a_increment::Response {
+        let (tx, rx) = ::tokio::sync::oneshot::channel();
+        client.tx.send(Message::Increment(req, tx)).await.unwrap();
+        rx.await.unwrap()
+    }
+
+}
--- servify_macro/src/service.rs
+++ servify_macro/src/service.rs
@@ -1,12 +1,14 @@
 use proc_macro2::TokenStream;
 use syn::parse::Parse;
 use syn::parse::ParseStream;
+use syn::parse2;
 use syn::Error;
+use syn::Ident;
 use syn::Result;
 
 pub(crate) fn impl_service(attrs: TokenStream, item: TokenStream) -> TokenStream {
-    syn::parse2::<ServiceParent>(item)
-        .map(|i| i.0)
+    parse2::<ServiceParentAttrs>(attrs)
+        .and_then(|attrs| attrs.parse_item(item))
         .unwrap_or_else(Error::into_compile_error)
 }
 
@@ -14,14 +16,21 @@ pub struct ServiceParentAttrs {}
 
 impl Parse for ServiceParentAttrs {
     fn parse(input: ParseStream) -> Result<Self> {
-        todo!()
+        while !input.is_empty() {
+            let property_name: Ident = input.parse()?;
+            match property_name.to_string().as_str() {
+                "impls" => {
+                    
+                } 
+                _ => return Err(Error::new(property_name.span(), "Unknown property. expected `impls`")),
+            }
+        }
+        Ok(Self {})
     }
 }
 
-pub struct ServiceParent(TokenStream);
-
-impl Parse for ServiceParent {
-    fn parse(input: ParseStream) -> Result<Self> {
-        todo!()
+impl ServiceParentAttrs {
+    fn parse_item(self, item: TokenStream) -> Result<TokenStream> {
+        Ok(item)
     }
 }
