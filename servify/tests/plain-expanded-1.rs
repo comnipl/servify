5f05c267be5a2d187566acf90f60bd025bde0e3d
AsPulse / あすぱる <contact@aspulse.dev>
2024-10-24 05:15:43 +0000

    add requirements trait

--- servify/tests/plain-expanded-1.rs
+++ servify/tests/plain-expanded-1.rs
@@ -1,8 +1,12 @@
-use servify::processor::ServifyProcessor;
-
 mod counter {
+    use std::any::Any;
+
     use servify::processor::ServifyProcessor;
-    
+
+    pub(super) trait ImplIncrementAndGet {
+        fn process(&mut self, ctx: Context, payload: impl Any) -> impl Any;
+    }
+
     pub(super) struct Processor {
         pub(super) count: u32,
     }
@@ -12,14 +16,30 @@ mod counter {
     }
 
     pub(super) enum Context {
+        Direct,
         MessagePassing,
     }
 
 }
 
-impl counter::Processor {
-    pub fn increment_and_get(&mut self, ctx: <Self as ServifyProcessor>::Context, amount: u32) -> u32 {
-        self.count += amount;
-        self.count
+mod counter_increment_and_get {
+    use super::counter;
+    use servify::processor::ServifyProcessor;
+
+    trait Process {
+        fn process(&mut self, ctx: <counter::Processor as ServifyProcessor>::Context, amount: u32) -> u32;
+    }
+
+    impl Process for counter::Processor {
+        fn process(&mut self, ctx: <Self as ServifyProcessor>::Context, amount: u32) -> u32 {
+            self.count += amount;
+            self.count
+        }
+    }
+
+    impl counter::Processor {
+        pub fn increment_and_get(&mut self, amount: u32) -> u32 {
+            self.process(counter::Context::Direct, amount)
+        }
     }
 }
