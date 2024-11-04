bcadb837036d7d82caa7c080cbc533412637cced
AsPulse / あすぱる <contact@aspulse.dev>
2024-11-04 22:16:40 +0900

    fix typo

--- servify_macro/src/export_macro/errors.rs
+++ servify_macro/src/export_macro/errors.rs
@@ -5,4 +5,4 @@ pub(super) const ERR_NO_FN_IN_EXPORT: &str =
     "One function must be exported in one impl block with servify::export";
 
 pub(super) const ERR_UNEXPECTED_ITEM_IN_EXPORT: &str =
-    "Supported items  are only exporting function, associated function, associated constants, and Request struct";
+    "Supported items are only exporting function, associated function, associated constants, and Request struct";
